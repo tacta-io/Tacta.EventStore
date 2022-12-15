b5afe27aae1e14df06d919ca70aab45632b0910c
nusretasinanovic <nusreta.sinanovic@tacta.io>
2022-12-15 11:57:25 +0100

    resilence unit test

---
+++ Tacta.EventStore.Test/Projector/DomainEvents/UserVerified.cs
@@ -0,0 +1,9 @@
+﻿using Tacta.EventStore.Domain;
+
+namespace Tacta.EventStore.Test.Projector.DomainEvents
+{
+    public sealed class UserVerified : DomainEvent
+    {
+        public UserVerified(string aggregateId) : base(aggregateId) { }
+    }
+}
