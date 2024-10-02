d1133947f522e14405dffe6ffb1d6ffd3593c39c
Marko Sego <marko.sego@tacta.io>
2024-10-02 09:30:39 +0200

    tests

---
+++ Tacta.EventStore.Test/Domain/AggregateRoots/BacklogItemCustomDomainEvent.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using Tacta.EventStore.Domain;
+using Tacta.EventStore.Test.Domain.DomainEvents;
+using Tacta.EventStore.Test.Domain.Entities;
+using Tacta.EventStore.Test.Domain.Identities;
+
+namespace Tacta.EventStore.Test.Domain.AggregateRoots
+{
+    public class BacklogItemCustomDomainEvent : AggregateRoot<BacklogItemId>
+    {
+        public override BacklogItemId Id { get; protected set; }
+
+        public string Summary { get; private set; }
+
+        public List<SubTask> SubTasks { get; private set; } = new List<SubTask>();
+
+        private BacklogItemCustomDomainEvent() { }
+
+        public BacklogItemCustomDomainEvent(IReadOnlyCollection<IDomainEvent> events) : base(events)
+        {
+        }
+
+        public static BacklogItemCustomDomainEvent FromSummary(BacklogItemId id, string summary)
+        {
+            var item = new BacklogItemCustomDomainEvent();
+
+            item.Apply(new BacklogItemCreated(summary, id));
+
+            return item;
+        }
+
+        public void On(BacklogItemCreated @event)
+        {
+            Id = @event.BacklogItemId;
+            Summary = @event.Summary;
+        }
+    }
+}
---
+++ Tacta.EventStore.Test/Domain/DomainEvents/BacklogItemCreatedCustomDomainEvent.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Newtonsoft.Json;
+using Tacta.EventStore.Test.Domain.Identities;
+
+namespace Tacta.EventStore.Test.Domain.DomainEvents
+{
+    public class BacklogItemCreatedCustomDomainEvent : CustomDomainEvent
+    {
+        public BacklogItemId BacklogItemId { get; }
+        public string Summary { get; }
+        
+        public BacklogItemCreatedCustomDomainEvent(string customDomainEventField, BacklogItemId backlogItemId, 
+            string summary) : base(customDomainEventField)
+        {
+            BacklogItemId = backlogItemId;
+            Summary = summary;
+        }
+
+        [JsonConstructor]
+        public BacklogItemCreatedCustomDomainEvent(
+            Guid id, 
+            DateTime createdAt,
+            string customDomainEventProperty,
+            BacklogItemId backlogItemId,
+            string summary
+            ) : base(id, createdAt, customDomainEventProperty)
+        {
+            BacklogItemId = backlogItemId;
+            Summary = summary;
+        }
+    }
+}
