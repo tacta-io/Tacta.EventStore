9529eaa1f42797b5e2f6f40c0fdbcbb9114e93d4
Ana-Marija <ana-marija.knezevic@constructiv.be>
2025-06-05 13:58:00 +0200

    Rename Audit table

---
+++ AuditLog.sql
@@ -0,0 +1,11 @@
+﻿IF OBJECT_ID(N'dbo.AuditLog', N'U') IS NULL 
+BEGIN
+    CREATE TABLE [dbo].AuditLog (
+        [Sequence] BIGINT NOT NULL,
+        [AppliedAt] DATETIME2(7) NOT NULL
+        );
+
+    DROP INDEX IF EXISTS [SequenceIndex] ON [dbo].[AuditLog];
+
+    CREATE NONCLUSTERED INDEX [SequenceIndex] ON [dbo].[AuditLog] ([Sequence]);
+END;
